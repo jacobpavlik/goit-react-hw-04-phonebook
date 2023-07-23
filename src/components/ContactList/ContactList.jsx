999c006dbc8d4904bababcf0078c612afbefd5df
Jakub Pawlik <jakub.pawlik@vp.pl>
2023-07-23 19:38:07 +0200

    Added files from HW03

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,49 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { ContactListItem } from 'components/ContactListItem/ContactListItem';
+import css from './ContactList.module.css';
+export class ContactList extends Component {
+  render() {
+    const { contacts, deleteContact, filter } = this.props;
+    console.log('contacts.length', contacts.length);
+    if (contacts.length === 0) {
+      return <p className={css.info}>Contacts list is empty</p>;
+    } else {
+      if (filter === '') {
+        return (
+          <div>
+            <ul>
+              {contacts.map((contact, index) => (
+                <ContactListItem
+                  key={index}
+                  contact={contact}
+                  deleteContact={deleteContact}
+                />
+              ))}
+            </ul>
+          </div>
+        );
+      } else {
+        const filteredContacts = contacts.filter(contact =>
+          contact.name.toLowerCase().includes(filter.toLowerCase())
+        );
+        return (
+          <ul>
+            {filteredContacts.map((contact, index) => (
+              <ContactListItem
+                key={index}
+                contact={contact}
+                deleteContact={deleteContact}
+              />
+            ))}
+          </ul>
+        );
+      }
+    }
+  }
+}
+ContactList.propTypes = {
+  contacts: PropTypes.array,
+  deleteContact: PropTypes.func,
+  filter: PropTypes.string,
+};
---
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+import css from './ContactListItem.module.css';
+
+export const ContactListItem = ({ contact, deleteContact, index }) => (
+  <li>
+    <div className={css.listItemContainer}>
+      <p className={css.contactName}>{contact.name}:</p>
+
+      <p className={css.contactName}> {contact.number}</p>
+
+      <button className={css.deleteButton} onClick={() => deleteContact(index)}>
+        Delete
+      </button>
+    </div>
+  </li>
+);
+ContactListItem.propTypes = {
+  contacts: PropTypes.array,
+  deleteContact: PropTypes.func,
+};
